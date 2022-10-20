4fd7d1c97820c0ff49f77094cdddfc3ccb6a8fa2
Dmytro Martus <funbrainfun@gmail.com>
2022-10-20 03:09:36 +0300

    planets

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 $(function () {
-  let menuOverflow = $('.menu-overflow');
-  $('.menu__list-item').each(function () {
-    menuOverflow.append($(this));
-  });
-
-  $('.menu__btn').on('click', function () {
-    $(this).toggleClass('menu__btn--active');
-    menuOverflow.toggleClass('menu-overflow--active');
-  });
-  // $('.menu__list-link').on('click', function () {
-  //   $('.menu__btn').removeClass('menu__btn--active');
-  //   $('.menu__list').removeClass('menu__list--active');
-  // });
+    $('.menu__btn').on('click', function () {
+        $(this).toggleClass('menu__btn--active');
+        menuOverflow.toggleClass('menu-overflow--active');
+    });
+    // $('.menu__list-link').on('click', function () {
+    //   $('.menu__btn').removeClass('menu__btn--active');
+    //   $('.menu__list').removeClass('menu__list--active');
+    // });
 });
